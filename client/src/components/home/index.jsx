b683bc6c2d767f658c8902b43a000efda765a8bf
SHUBHAM SINGH <106802656+TheShubham1008@users.noreply.github.com>
2024-05-09 21:56:46 +0530

    Update index.jsx

--- client/src/components/home/index.jsx
+++ client/src/components/home/index.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
-import { useAuth } from '../../contexts/authContext'
-// {currentUser.displayName ? currentUser.displayName : currentUser.email}
-const Home = () => {
-    const { currentUser } = useAuth()
-    return (
-        <>
-        <div className='text-2xl font-bold pt-14'>Name :{currentUser.displayName}</div>
-        <div className='text-2xl font-bold pt-14'>email :{currentUser.email}</div>
-        <div className='text-2xl font-bold pt-14'>phone :{currentUser.phone}</div>
-        <div className='text-2xl font-bold pt-14'>profile photo :{currentUser.photoUrl}</div>
-        </>
-    )
-}
+import React from "react";
+import { useAuth } from "../../contexts/authContext";
+import QRCode from "react-qr-code";
 
-export default Home
+const Home = () => {
+  const { currentUser } = useAuth();
+
+  return (
+    <>
+      <div className="flex flex-row items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center h-screen">
+          <div className="text-2xl font-bold pt-14">
+            Name:{" "}
+            <span className="text-gray-700">
+              {currentUser.displayName || "Not available"}
+            </span>
+          </div>
+          <div className="text-2xl font-bold pt-14">
+            Email: <span className="text-gray-700">{currentUser.email}</span>
+          </div>
+          <div className="text-2xl font-bold pt-14">
+            Phone:{" "}
+            <span className="text-gray-700">
+              {currentUser.phone || "Not available"}
+            </span>
+          </div>
+          <div className="text-2xl font-bold pt-14">
+            Profile Photo:{" "}
+            <span className="text-gray-700">
+              {currentUser.photo || "Not available"}
+            </span>
+          </div>
+        </div>
+        <div  className="ml-14">
+          <QRCode value={currentUser.displayName || currentUser.email} size={230} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
